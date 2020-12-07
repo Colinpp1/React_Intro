bfcd8f009ad974ab3e701c3a75037d44a11fe4d6
Colinpp1 <colinpp1@gmail.Com>
2020-12-07 12:01:12 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("h1", null, " This is a Component "), document.querySelector('#root'));
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1> This is a React Component </h1>,
+    document.querySelector('#root')
+)
